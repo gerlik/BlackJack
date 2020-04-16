c96bb8c96804654b42f65c8967b7ab342b3102ca
gerlik <gerliks@gmail.com>
2020-04-16 23:12:02 +0300

    Fix folder structure, GUI

--- src/main/java/oop/Main.java
+++ src/main/java/oop/Main.java
@@ -1,13 +1,20 @@
 package oop;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.File;
+
+
 public class Main extends Application {
+
 
     public static void main(String[] args) {
         launch(args);
@@ -15,20 +22,33 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
         GridPane grid = new GridPane();
+        VBox introduction = new VBox();
+        introduction.setSpacing(10);
+
+        // Add background image or color
+        Image img = new Image(new File("background-image.jpg").toURI().toString());
+//        Image img = new Image("file:background-image.jpg", 320, 320, false, true);
+        BackgroundImage bg_img = new BackgroundImage(img, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT,
+                BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+
+        BackgroundFill bg_fill = new BackgroundFill(Color.FORESTGREEN, CornerRadii.EMPTY, Insets.EMPTY);
+//        Background bg = new Background(bg_img);
+        Background bg = new Background(bg_fill);
+        introduction.setBackground(bg);
 
         Text askName = new Text("Hello, what is your name? ");
-        grid.getChildren().add(askName);
+        TextField name = new TextField("Enter name");
+        Text askMoneyAmount = new Text("How much cash you have today? ");
+        TextField money = new TextField("0.00");
+
+//        Text sayHello = new Text("Hello, " + name + " we hope you are ready to play some blackjack.");
+        // Add introduction text to pane
+        introduction.getChildren().addAll(askName, name, askMoneyAmount, money);
+        grid.getChildren().add(introduction);
+
         int games_played = 0;
-//        Scanner scan = new Scanner(System.in);
-//        System.out.println("Hello, what is your name? ");
-//        String name = "hey";
-
-//        System.out.println("How much cash you have today? ");
-//        double money = Double.parseDouble(scan.nextLine());
-//        double startMoney = money;
-
-//        System.out.println("Hello, " + name + " we hope you are ready to play some blackjack.\n");
 
         // Gameplay loop
 //        while (money > 0) {
@@ -72,9 +92,10 @@ public class Main extends Application {
 //            System.out.printf("You lost %s, thank you for playing.", difference);
 //        }
 
-        Scene scene = new Scene(grid, 500, 650, Color.SNOW);  // luuakse stseen
+        Scene scene = new Scene(grid, 500, 500);
         stage.setTitle("Blackjack");
         stage.setScene(scene);
         stage.show();
     }
+
 }
