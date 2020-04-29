945f30799f9c2e6a593d37d69febabed0b73a546
gerlik <gerliks@gmail.com>
2020-04-29 18:46:54 +0300

    Make separate game window if payer has entered some information

--- src/main/java/oop/Main.java
+++ src/main/java/oop/Main.java
@@ -1,16 +1,14 @@
 package oop;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import java.io.File;
 
 
 public class Main extends Application {
@@ -24,29 +22,42 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
 
         GridPane grid = new GridPane();
+        grid.setStyle("-fx-background-color: #228B22;");  // Set intro panel bg to green
         VBox introduction = new VBox();
         introduction.setSpacing(10);
 
-        // Add background image or color
-        Image img = new Image(new File("background-image.jpg").toURI().toString());
-//        Image img = new Image("file:background-image.jpg", 320, 320, false, true);
-        BackgroundImage bg_img = new BackgroundImage(img, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT,
-                BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
-
-        BackgroundFill bg_fill = new BackgroundFill(Color.FORESTGREEN, CornerRadii.EMPTY, Insets.EMPTY);
-//        Background bg = new Background(bg_img);
-        Background bg = new Background(bg_fill);
-        introduction.setBackground(bg);
-
-        Text askName = new Text("Hello, what is your name? ");
+        Text hello = new Text("Hello, we hope you are ready to play some blackjack.\n\r");
+        Text askName = new Text("What is your name?");
         TextField name = new TextField("Enter name");
-        Text askMoneyAmount = new Text("How much cash you have today? ");
+        Text askMoneyAmount = new Text("How much cash you have today?");
         TextField money = new TextField("0.00");
+        Text gameInfoText = new Text("\n\rGame starts after you press Enter");
 
-//        Text sayHello = new Text("Hello, " + name + " we hope you are ready to play some blackjack.");
-        // Add introduction text to pane
-        introduction.getChildren().addAll(askName, name, askMoneyAmount, money);
+        // Add introduction text to pane and set alignment
+        introduction.getChildren().addAll(hello, askName, name, askMoneyAmount, money,gameInfoText); // Add all text and textboxes
         grid.getChildren().add(introduction);
+        grid.setAlignment(Pos.CENTER);
+        introduction.setAlignment(Pos.CENTER);
+        introduction.setStyle("-fx-font: 18 arial;"); // Change fot of VBox
+        // TODO Rule page maybe
+
+        // Make gameplay window
+        money.setOnKeyPressed(key -> { // If enter pressed open game window
+            if (key.getCode().equals(KeyCode.ENTER)) {
+                VBox gameWindow = new VBox();
+                Text dealer = new Text("Dealer");
+                Text you = new Text("You (" + (name.getText()) + ")");
+                Text wallet = new Text("Wallet "+money.getText());
+                gameWindow.getChildren().addAll(dealer, you,wallet);
+                gameWindow.setAlignment(Pos.CENTER);
+                gameWindow.setStyle("-fx-background-color: #228B22;");
+
+                Scene gameScene = new Scene(gameWindow, 900, 600);
+                stage.setTitle("Blackjack");
+                stage.setScene(gameScene);
+                stage.show();
+            }
+        });
 
         int games_played = 0;
 
@@ -92,7 +103,7 @@ public class Main extends Application {
 //            System.out.printf("You lost %s, thank you for playing.", difference);
 //        }
 
-        Scene scene = new Scene(grid, 500, 500);
+        Scene scene = new Scene(grid, 900, 600);
         stage.setTitle("Blackjack");
         stage.setScene(scene);
         stage.show();
